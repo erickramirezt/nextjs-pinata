66e2b77dad31234425a7423fbbd6cf3bb3d90534
Erick Ramirez <erickramirez.dnbk@gmail.com>
2025-09-01 07:07:53 -0400

    feat: :sparkles: create upload file feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
-import Counter from "./_components/counter";
+'use client'
 
-export default function Page() {
+import { pinata } from '@/lib/pinata.config'
+import { useState } from 'react'
+
+export default function Home() {
+  const [file, setFile] = useState<File>()
+  const [uploading, setUploading] = useState(false)
+
+  const uploadFile = async () => {
+    if (!file) {
+      alert('No file selected')
+      return
+    }
+
+    try {
+      setUploading(true)
+      const urlRequest = await fetch('/api/url') // Fetches the temporary upload URL
+      const urlResponse = await urlRequest.json() // Parse response
+      const upload = await pinata.upload.public.file(file).url(urlResponse.url) // Upload the file with the signed URL
+      console.log(upload)
+      setUploading(false)
+      alert('File uploaded successfully')
+    } catch (e) {
+      console.log(e)
+      setUploading(false)
+      alert('Trouble uploading file')
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(e.target?.files?.[0])
+  }
+
   return (
-    <main className="flex items-center justify-center min-h-screen gap-2">
-      <Counter />
+    <main className='w-full min-h-screen m-auto flex flex-col justify-center items-center'>
+      <input type='file' onChange={handleChange} />
+      <button type='button' disabled={uploading} onClick={uploadFile}>
+        {uploading ? 'Uploading...' : 'Upload'}
+      </button>
     </main>
-  );
+  )
 }
