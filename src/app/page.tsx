5c8a21ec4591f5f9f8d3ba40873649be9e663517
Erick Ramirez <erickramirez.dnbk@gmail.com>
2025-09-01 21:53:47 -0400

    fix: :lock: add group to upload files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { pinata } from '@/lib/pinata.config'
 import { useState } from 'react'
 
@@ -17,7 +18,10 @@ export default function Home() {
       setUploading(true)
       const urlRequest = await fetch('/api/url') // Fetches the temporary upload URL
       const urlResponse = await urlRequest.json() // Parse response
-      const upload = await pinata.upload.public.file(file).url(urlResponse.url) // Upload the file with the signed URL
+      const upload = await pinata.upload.public
+        .file(file)
+        .url(urlResponse.url)
+        .group(String(process.env.NEXT_PUBLIC_PINATA_GROUP_ID))
       console.log(upload)
       setUploading(false)
       alert('File uploaded successfully')
@@ -35,9 +39,9 @@ export default function Home() {
   return (
     <main className='w-full min-h-screen m-auto flex flex-col justify-center items-center'>
       <input type='file' onChange={handleChange} />
-      <button type='button' disabled={uploading} onClick={uploadFile}>
+      <Button type='button' disabled={uploading} onClick={uploadFile}>
         {uploading ? 'Uploading...' : 'Upload'}
-      </button>
+      </Button>
     </main>
   )
 }
